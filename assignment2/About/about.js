8c96d15e7253faa51bbdf88656c06c07aaa0a080
jannalyntan <s4056775@student.rmit.edu.au>
2025-09-18 14:37:46 +1000

    update

---
+++ assignment2/About/about.js
@@ -0,0 +1,27 @@
+document.getElementById("projectBtn").addEventListener("click", () => {
+  document.getElementById("projectCon").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("techBtn").addEventListener("click", () => {
+  document.getElementById("techCon").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("peerBtn").addEventListener("click", () => {
+  document.getElementById("userTesting").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("projectBtn1").addEventListener("click", () => {
+  document.getElementById("projectCon").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("techBtn1").addEventListener("click", () => {
+  document.getElementById("techCon").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("peerBtn1").addEventListener("click", () => {
+  document.getElementById("userTesting").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("backTopBtn").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
